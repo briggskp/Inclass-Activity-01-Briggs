540fb291c1b6b193c9e0237d5a1f28123fd3d68e
Kaelin Briggs <briggskp@g.cofc.edu>
2017-02-04 21:44:49 -0500

    Initial commit

---
+++ app/src/main/java/edu/cofc/inclass_activity_01_briggs/MainActivity.java
@@ -0,0 +1,12 @@
+package edu.cofc.inclass_activity_01_briggs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
